caaae5c0d9d737c3dfaa5091e44e64df77ce6e34
Mohammed Arqam <mohammedarqam97@gmail.com>
2019-02-18 10:59:23 +0530

    Not Usable

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,54 +1,57 @@
 import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
-var http = require("http");
+// import * as admin from 'firebase-admin';
+const http = require("http");
 
 export const bulkPromo = functions.database.ref('Promotions/To All/{promoId}').onCreate((snapshot, context) => {
     const promoDat = snapshot.val();
     const mess: string = promoDat.Message;
-    const phones: Array<any> = [];
 
+
+    const urr1 = "http://api.msg91.com/api/sendhttp.php?country=91&sender=BEGUMS&route=4&mobiles="
+    const phone = "9493726705";
+    const urr2 = "&authkey=248515ASS3bXdTM6iH5bf6582b&message=";
+    const urr3 = "Janata local2";
+    const fU = urr1 + phone + urr2 + urr3;
+    const fu1 = encodeURI(fU);
+    http.get(fu1);
+    console.log(mess);
 
     // admin.initializeApp({
     //     credential: admin.credential.applicationDefault(),
     //     databaseURL: 'https://begums-crm.firebaseio.com/'
     // });
 
-    admin.app().database().ref("Users").once("value", itemSnap => {
-        itemSnap.forEach(snap => {
-            phones.push(snap.val().Phone);
-        })
+    // admin.app().database().ref("Users").once("value", itemSnap => {
+    //     itemSnap.forEach(snap => {
+    //         phones.push(snap.val().Phone);
+    //     })
 
 
 
-        console.log(phones);
-        console.log(mess);
+    // console.log(phones);
+    // console.log(mess);
 
-        for (let i = 0; i < phones.length; i++) {
-            const urr1 = "/api/sendhttp.php?country=91&sender=BEGUMS&route=4&mobiles="
-            const urr2 = "&authkey=248515ASS3bXdTM6iH5bf6582b&message=";
-            const phone = phones[i];
-            const fU = encodeURI(urr1 + phone + urr2 + mess);
-            var options = {
-                "method": "GET",
-                "hostname": "api.msg91.com",
-                "path": fU,
-            };
-            var req = http.request(options);
-            req.end();
-        }
-
-
-
-        return itemSnap.val();
-    }).catch(error => {
-        console.error(error);
-    })
+    //     const urr1 = "http://api.msg91.com/api/sendhttp.php?country=91&sender=BEGUMS&route=4&mobiles="
+    //     const phone = "9493726705";
+    //     const urr2 = "&authkey=248515ASS3bXdTM6iH5bf6582b&message=Lenovo";
+    //     const urr3 = mess;
+    //     const fU = urr1 + phone + urr2 + urr3;
+    //     http.get(fU, {
+    //         headers: {
+    //             "Access-Control-Allow-Origin": "*",
+    //             "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+    //         },
+    //     })
 
 
 
 
 
-    return snapshot.val()
+
+
+
+    return http.get(fu1);
+
 
 });
 
